478e50fdb2151a187b998ec9954e2fab8c0dc5a3
jessdowdell <jess.dowdell@bsu.edu>
2026-06-03 17:01:09 -0500

    Update Assignment4.py

--- Assignment4.py
+++ Assignment4.py
@@ -3,16 +3,20 @@ import requests
 #This is the first web service endpoint for Muncie, IN using lat and long.
 points_url = "https://api.weather.gov/points/40.1934,-85.3864"
 
-#Sends request to the first URL and gets JSON data
+#Sends request to the first URL
 response = requests.get(points_url)
+
+# Gets data from JSON and converts response to python dictionary
 data = response.json()
 
 # This is the second web service endpoint 
 # Gets forcast URL from inside the properties section
 forecast_url = data["properties"]["forecast"]
 
-# Sends request to forecast URl and gets JSON data
+# Sends request to forecast URl
 forecast_response = requests.get(forecast_url)
+
+# Gets data from JSON and converts response to python dictionary
 forecast_data = forecast_response.json()
 
 # Gets the list of forecast periods
